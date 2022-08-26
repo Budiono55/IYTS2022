d7182a6f233e114962699e34477857b61e3e7cbd
budiono55 <zakizaidan304@ymail.com>
2022-08-26 13:05:33 +0700

    riviw ver 1

---
+++ cards-review.js
@@ -0,0 +1,28 @@
+const cards = [
+    {
+        "name": "Alfarisi",
+        "img": "#",
+        "description": "<p>Seru banget! Apalagi ditemani kakak-kakak fasil yang gak kalah kerennya. Di IYTS ini, aku belajar banyak hal baru seperti design thinking, public speaking, dan menyusun inovasi ide dalam sebuah karya project. Semua itu aku dapatkan di IYTS 2022. Sampai jumpa di IYTS 2023!!</p>",
+        "role": "Kutipan peserta IYTS 2022",
+    }, {
+        "name": "Ananda Naufal",
+        "img": "#",
+        "description": "<p>Menurutku sendiri dengan adanya campaign tersebut bisa menjadi reminder buat diri sendiri dan juga ikut membantu men-trigger masyarakat muda lainnya untuk berani dalam mengembangkan diri dan mengejar impian. </p>",
+        "role": "Kutipan peserta IYTS 2022",
+    }, {
+        "name": "Zulfikar",
+        "img": "#",
+        "description": "<p>Awalnya, ekspektasi aku ketika mengikuti IYTS cuma bakal dapet ilmu baru aja. Tetapi setelah mengikuti rangkaian acaranya, ternyata aku dapet jauh lebih banyak dari itu. Mulai dari relasi dengan orang-orang hebat di seluruh penjuru Indonesia, lingkungan produktif bersama teman-teman baru, dan yang sangat penting adalah ilmu yang sangat relevan dengan kehidupan aku. Indeed engga sia-sia aku nungguin event ini delapan bulan lamanya. Terima kasih Rangers IYTS 2022!</p>",
+        "role": "Kutipan peserta IYTS 2022",
+    }, {
+        "name": "Aban",
+        "img": "#",
+        "description": "<p>Honestly, sesi Build Skills &#34; Design Thinking &#34; di IYTS 2022  bener-bener unexpected. Aku kira selama ini cara berpikir aku udah bener. Ternyata masih banyak kesalahan yang aku lakukan ketika mencoba berpikir atau memecahkan masalah. Di sesi ini kita bener-bener diajarin gimana cara menganalisis masalah dan mencoba mencari solusi terbaik dari masalah tersebut. Challenge yang berada di akhir sesi juga bikin kita jadi tambah paham dan inget apa yang udah kita pelajari. Thank you buat narasumber dan juga panitia IYTS yang udah ngasih materi yang bermanfaat buat kami kali ini. See you on top and keep progressing!!</p>",
+        "role": "Kutipan peserta IYTS 2022",
+    }, {
+        "name": "Kuncoro",
+        "img": "#",
+        "description": "<p>Saat mengikuti sesi Contribute to Indonesia ini benar-benar seru banget karena kebetulan saya diberikan kesempatan untuk menjadi Leader di kelompok saya sehingga skill leadership saya benar-benar di uji mulai pembuatan Woksheet, PPT, dan Latihan presentasi sangat membutuhkan kerja sama team yang sangat baik apalagi saat Pressentasi finalis kami semua harus saling mendukung dan menguatkan. <br> Dan akhirnya mendapatkan juara di Contribute to Indonesia. Saya berterima kasih kepada Indonesia Youth Transformation Summit untuk kesempatan yang luar biasa dan kelompok 8 yang bekerjasama baik dengan waktu singkat🙌</p>",
+        "role": "Kutipan peserta IYTS 2022",
+    }, 
+]
---
+++ carousel-rev.js
@@ -0,0 +1,155 @@
+let items = document.querySelectorAll('.carousel .carousel-item.rev-item')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+
+
+function anim(e){
+    let timeline_img = e.target
+    let card_img = timeline_img.parentElement
+    let card_name = timeline_img.classList[1]
+    let timeline_imgs = document.querySelectorAll(`.${timeline_img.classList[1]}`)
+
+    timeline_imgs.forEach((el) => {
+        let dtl = document.createElement('div')
+        dtl.classList.add('timeline-detail')
+
+        let el_card = el.parentElement
+        dtl = el_card.insertBefore(dtl, el)
+        dtl.setAttribute('style', `width: ${timeline_img.width}px;height:${card_img.clientHeight}px;`)
+
+        let dtl_overlay = document.createElement('div')
+        dtl_overlay.classList.add('timeline-detail-overlay')
+
+        // animation
+        el.setAttribute('style', 'left: -100%')
+        card_img.setAttribute('style', `width: ${timeline_img.width}px`)
+
+        // add description div
+        let title = document.createElement('h3')
+        let desc = document.createElement('div')
+        desc.classList.add('card-desc')
+
+        let learn = document.createElement('div')
+        learn.classList.add("card-learn")
+        learn.innerHTML = "<a>Learn More</a>"
+        learn.querySelector('a').setAttribute('style', 'z-index: 0')
+
+        cards.forEach((card) => {
+            if (card.name == card_name){
+                title.innerHTML = card.title
+                desc.innerHTML = card.description
+                learn.querySelector('a').href = card.link
+            }
+        })
+
+        dtl.appendChild(title)
+        dtl.appendChild(desc)
+        dtl_overlay = dtl.appendChild(dtl_overlay)
+        dtl.appendChild(learn)
+
+        // end
+
+        window.setTimeout(() => {
+            learn.querySelector('a').removeAttribute('style')
+            card_img.removeAttribute('style')
+            dtl_overlay.setAttribute('onclick', 'animReverse(event)')
+        }, 500)
+    })
+
+}
+
+
+
+function animReverse(e){
+
+    let timeline_detail = e.target.parentElement
+
+    let timeline_img = timeline_detail.nextSibling
+
+    let timeline_imgs = document.querySelectorAll(`.${timeline_img.classList[1]}`)
+
+
+
+    timeline_imgs.forEach((el) => {
+
+        let el_card = el.parentElement
+
+        let dtl = el.previousElementSibling
+
+        let learn = dtl.querySelector('.card-learn').querySelector('a').setAttribute('style', 'z-index: 0')
+
+        // animation
+
+        el_card.setAttribute('style', `width: ${timeline_img.width}px`)
+
+        el.removeAttribute('style')
+
+
+        //end
+
+        window.setTimeout(() => {
+
+            dtl.remove()
+
+            el_card.removeAttribute('style')
+
+        }, 500)
+
+    })
+
+}
+
+
+
+function resizeCard(){
+
+    let timeline_img = document.querySelector('.active .img-fluid')
+
+    let imgs = Array.from(document.querySelectorAll('.img-fluid'))
+
+
+
+    let img = imgs.filter(function(e){
+
+        try{
+
+            return e.previousSibling.classList[0] == 'timeline-detail'
+
+        }
+
+        catch(err){
+
+            //nothing
+
+        }
+
+    })
+
+
+
+    img.forEach((el) => {
+
+            let dtl = el.previousElementSibling
+
+            dtl.setAttribute('style', `width: ${timeline_img.width}px`)  
+
+    })
+
+}
+
+
+
+window.addEventListener('resize', resizeCard)
